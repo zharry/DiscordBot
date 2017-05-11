ccd3fd481480486dd65770213367d2ed223cc24f
Harry Zhang <zh.harry@yahoo.ca>
2017-05-11 19:57:06 -0400

    Lol, it works now...

--- src/BotListener.java
+++ src/BotListener.java
@@ -4,11 +4,35 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class BotListener extends ListenerAdapter {
 
 	@Override
-	public void onMessageReceived(MessageReceivedEvent event) {
-		if (event.getMessage().getContent().startsWith(DiscordBot.PREFIX)) {
-			if (event.getMessage().getAuthor().getId() != event.getJDA().getSelfUser().getId()) {
-				DiscordBot.handleCommand(DiscordBot.parser.parse(event.getMessage().getContent().toLowerCase(), event));
+	public void onMessageReceived(MessageReceivedEvent e) {
+
+		String message = e.getMessage().getRawContent().toLowerCase();
+		if (message.startsWith(DiscordBot.BOT_PREFIX)) {
+			message = message.substring(1);
+			String cmd = splitCommand(message)[0], args = splitCommand(message)[1];
+
+			String returnMessage = "";
+			switch (cmd) {
+
+			case "ping":
+				returnMessage = "Pong!";
+				break;
+			case "debug":
+				returnMessage = " Command: " + cmd + ", Arguments: " + args;
+				break;
+			default:
+				returnMessage = " Sorry '" + message + "', is not a command";
+
 			}
+			e.getChannel().sendMessage(e.getAuthor().getAsMention() + returnMessage).queue();
 		}
 	}
+
+	public String[] splitCommand(String command) {
+		String[] proc = command.split(" ");
+		String cmd = proc[0];
+		String args = command.replaceFirst(cmd + " ", "");
+		return new String[] { cmd, args };
+	}
+
 }
--- src/DiscordBot.java
+++ src/DiscordBot.java
@@ -1,43 +1,23 @@
-import java.util.HashMap;
-
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 
 public class DiscordBot {
 
-	private static JDA jda;
-	public static final CommandParser parser = new CommandParser();
-	public static final String TOKEN = "MzEyMzQ4NjE5MjE2OTEyMzg1.C_Zxew.kT4SLfnluxZYTbe9-ZTgis8HA4Q";
-	public static final String PREFIX = ">";
+	public static JDA jda;
 
-	public static HashMap<String, Command> commands = new HashMap<String, Command>();
+	public static final String BOT_TOKEN = "MzEyMzQ4NjE5MjE2OTEyMzg1.C_aGcQ.lWJV2QDFO9ZxjuAdQKtmzDFIHbY";
+	public static final String BOT_PREFIX = ">";
 
 	public static void main(String[] args) {
 
-		// Add Commands
-		commands.put("ping", new Ping());
-
 		try {
-			// Configure JDA
-			jda = new JDABuilder(AccountType.BOT).addEventListener(new BotListener()).setToken(TOKEN).buildBlocking();
-			jda.setAutoReconnect(true);
-
+			jda = new JDABuilder(AccountType.BOT).addEventListener(new BotListener()).setToken(BOT_TOKEN)
+					.buildBlocking();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-	}
-
-	public static void handleCommand(CommandParser.CommandContainer cmd) {
-		if (commands.containsKey(cmd.invoke)) {
-			boolean safe = commands.get(cmd.invoke).called(cmd.args, cmd.event);
-
-			if (safe) {
-				commands.get(cmd.invoke).action(cmd.args, cmd.event);
-			}
-			commands.get(cmd.invoke).executed(safe, cmd.event);
-		}
 	}
 
 }
