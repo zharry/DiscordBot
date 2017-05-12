6d7a55a2b1a8fdd946d867b9df22b07885855fb2
Harry Zhang <zh.harry@yahoo.ca>
2017-05-12 17:48:56 -0400

    Final push for production testing

--- src/DiscordBot.java
+++ src/DiscordBot.java
@@ -11,6 +11,7 @@ public class DiscordBot {
 
 	public static void main(String[] args) {
 
+		System.out.println("Welcome to Harry's Discord Bot!");
 		System.out.println("Prefix is " + BOT_PREFIX);
 
 		try {
