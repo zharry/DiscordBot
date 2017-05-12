1c27b3d9b71dfbcc36d72ec1053feef64f8bb8a3
Harry Zhang <zh.harry@yahoo.ca>
2017-05-12 17:50:41 -0400

    Added Versioning

--- src/DiscordBot.java
+++ src/DiscordBot.java
@@ -6,12 +6,13 @@ public class DiscordBot {
 
 	public static JDA jda;
 
+	public static final int VERSION = 1;
 	public static final String BOT_TOKEN = "MzEyMzQ4NjE5MjE2OTEyMzg1.C_aGcQ.lWJV2QDFO9ZxjuAdQKtmzDFIHbY";
 	public static final String BOT_PREFIX = ">";
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Harry's Discord Bot!");
+		System.out.println("Welcome to Harry's Discord Bot! Version " + VERSION);
 		System.out.println("Prefix is " + BOT_PREFIX);
 
 		try {
