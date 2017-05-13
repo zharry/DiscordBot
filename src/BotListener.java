d7bd58995ad051b8e0753e79ca2b9a045e16d13f
Harry Zhang <zh.harry@yahoo.ca>
2017-05-13 00:15:05 -0400

    Revamped JSON and Agora.gg system

--- src/BotListener.java
+++ src/BotListener.java
@@ -4,7 +4,13 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
@@ -22,7 +28,10 @@ public class BotListener extends ListenerAdapter {
 
 			String returnMessage = "";
 			switch (cmd) {
-
+			case "prefix":
+				DiscordBot.BOT_PREFIX = args.charAt(0) + "";
+				returnMessage = "Prefix changed to " + args.charAt(0) + ".";
+				break;
 			case "ping":
 				returnMessage = "Pong!";
 				break;
@@ -37,14 +46,15 @@ public class BotListener extends ListenerAdapter {
 				}
 				break;
 			case "help":
-				returnMessage = "\nBot Commands:\n" 
-						+ DiscordBot.BOT_PREFIX + "help: Displays this message\n"
-						+ DiscordBot.BOT_PREFIX + "ping: Pings the bot\n";
-				returnMessage += "\nParagon Bot Commands:\n"
-						+ DiscordBot.BOT_PREFIX + "elo [username]: Username's ELO, W/L and KDA according to Agora.gg\n";
+				returnMessage = "\nBot Commands:\n" + DiscordBot.BOT_PREFIX + "help: Displays this message\n"
+						+ DiscordBot.BOT_PREFIX + "ping: Pings the bot\n" + DiscordBot.BOT_PREFIX
+						+ "prefix [prefix]: Changes the bot prefix\n";
+				returnMessage += "\nParagon Bot Commands:\n" + DiscordBot.BOT_PREFIX
+						+ "elo [username]: Username's ELO, W/L and KDA according to Agora.gg\n";
 				break;
 			default:
-				returnMessage = "Sorry '" + message + "', is not a command.\nSee " + DiscordBot.BOT_PREFIX + "help for a list of commands.";
+				returnMessage = "Sorry '" + message + "', is not a command.\nSee " + DiscordBot.BOT_PREFIX
+						+ "help for a list of commands.";
 
 			}
 			e.getChannel()
@@ -54,69 +64,80 @@ public class BotListener extends ListenerAdapter {
 
 	public String getElo(String username) throws Exception {
 
-		// Search for User
-		URLConnection agora = new URL("https://api.agora.gg/players/search/" + username).openConnection();
-		agora.setRequestProperty("User-Agent",
-				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-		agora.connect();
-		BufferedReader r = new BufferedReader(new InputStreamReader(agora.getInputStream(), Charset.forName("UTF-8")));
-		String fetchUserIdProc, fetchUserId = "";
-		while ((fetchUserIdProc = r.readLine()) != null)
-			fetchUserId += fetchUserIdProc;
-		r.close();
-		JSONObject json = new JSONObject(fetchUserId);
+		String message = "```\n" + username + "\n--------\n";
+
 		try {
-			json = new JSONObject(json.get("data").toString().substring(1, json.get("data").toString().length() - 1));
+			// Search for User
+			URLConnection agora = new URL("https://api.agora.gg/players/search/" + username).openConnection();
+			agora.setRequestProperty("User-Agent",
+					"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+			agora.connect();
+			BufferedReader r = new BufferedReader(
+					new InputStreamReader(agora.getInputStream(), Charset.forName("UTF-8")));
+			String fetchUserIdProc, fetchUserId = "";
+			while ((fetchUserIdProc = r.readLine()) != null)
+				fetchUserId += fetchUserIdProc;
+			r.close();
+
+			// Locate Player ID
+			boolean found = false;
+			try {
+				JSONArray json = new JSONArray(new JSONObject(fetchUserId).getJSONArray("data").toString());
+				for (int i = 0; i < json.length(); i++) {
+					Map<String, Object> idsMap = toMap((JSONObject) json.get(i));
+					int playerID = Integer.parseInt(idsMap.get("id").toString());
+
+					// Search for Player ID Data
+					URLConnection agoraPlayers = new URL("https://api.agora.gg/players/" + playerID).openConnection();
+					agoraPlayers.setRequestProperty("User-Agent",
+							"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+					agoraPlayers.connect();
+					BufferedReader in = new BufferedReader(
+							new InputStreamReader(agoraPlayers.getInputStream(), Charset.forName("UTF-8")));
+					String fetchUserDataProc, fetchUserData = "";
+					while ((fetchUserDataProc = in.readLine()) != null)
+						fetchUserData += fetchUserDataProc;
+					r.close();
+
+					// Check Profile Status
+					JSONArray data = new JSONObject(fetchUserData).getJSONObject("data").getJSONArray("stats");
+					if (new JSONObject(fetchUserData).getJSONObject("data").get("privacyEnabled").toString()
+							.equals("true")) {
+						message += "User's profile is hidden.\n--------\n";
+						found = true;
+					} else {
+						// Find Player's PVP Stats
+						for (int j = 0; j < data.length(); j++) {
+							Map<String, Object> statsMap = toMap((JSONObject) data.get(j));
+							if (Integer.parseInt(statsMap.get("mode").toString()) == 4) {
+								found = true;
+
+								// Calculate Player Info
+								double elo = Double.parseDouble(statsMap.get("elo").toString());
+								double wins = Double.parseDouble(statsMap.get("wins").toString());
+								double games = Double.parseDouble(statsMap.get("gamesPlayed").toString());
+								double wl = games == 0 ? 0 : wins / games;
+								double kills = Double.parseDouble(statsMap.get("kills").toString());
+								double deaths = Double.parseDouble(statsMap.get("deaths").toString());
+								double assists = Double.parseDouble(statsMap.get("assists").toString());
+								double kda = deaths == 0 ? 0 : (kills + assists) / deaths;
+
+								DecimalFormat format = new DecimalFormat("#.00");
+								message += "ELO: " + format.format(elo) + "\nW/L: " + format.format(wl * 100)
+										+ "%\nKDA: " + format.format(kda) + "\n--------\n";
+							}
+						}
+					}
+				}
+				if (!found)
+					message += "No PVP Games on record.\n--------\n";
+			} catch (Exception e) {
+				message += "User not found.\n--------\n";
+			}
 		} catch (Exception e) {
-			return "User " + username + " not found!";
+			message += "Something went wrong, please try again.\n--------\n";
 		}
-		String ID = json.get("id").toString();
-
-		// Return User Information
-		URLConnection player = new URL("https://api.agora.gg/players/" + ID).openConnection();
-		player.setRequestProperty("User-Agent",
-				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-		player.connect();
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(player.getInputStream(), Charset.forName("UTF-8")));
-		String fetchUserDataProc, fetchUserData = "";
-		while ((fetchUserDataProc = in.readLine()) != null)
-			fetchUserData += fetchUserDataProc;
-		r.close();
-
-		JSONObject data = new JSONObject(fetchUserData);
-		data = new JSONObject(
-				"{" + data.get("data").toString().substring(1, data.get("data").toString().length() - 1) + "}");
-		if (data.get("privacyEnabled").toString().equals("true")) {
-			return "User " + username + "'s profile is hidden!";
-		}
-		String stats = fetchUserData;
-		int i = stats.indexOf("\"mode\":4");
-		int eloLoc = stats.indexOf("\"elo\":", i);
-		int eloLocEnd = stats.indexOf(",\"", eloLoc);
-		int gamesLoc = stats.indexOf("\"gamesPlayed\":", i);
-		int gamesLocEnd = stats.indexOf(",\"", gamesLoc);
-		int winsLoc = stats.indexOf("\"wins\":", i);
-		int winsLocEnd = stats.indexOf(",\"", winsLoc);
-		int killsLoc = stats.indexOf("\"kills\":", i);
-		int killsLocEnd = stats.indexOf(",\"", killsLoc);
-		int deathsLoc = stats.indexOf("\"deaths\":", i);
-		int deathsLocEnd = stats.indexOf(",\"", deathsLoc);
-		int assistsLoc = stats.indexOf("\"assists\":", i);
-		int assistsLocEnd = stats.indexOf(",\"", assistsLoc);
-
-		DecimalFormat format = new DecimalFormat("#.00");
-		double elo = Double.parseDouble(stats.substring(eloLoc + 6, eloLocEnd));
-		double games = Double.parseDouble(stats.substring(gamesLoc + 14, gamesLocEnd));
-		double wins = Double.parseDouble(stats.substring(winsLoc + 7, winsLocEnd));
-		double wl = (wins / games) * 100;
-		double kills = Double.parseDouble(stats.substring(killsLoc + 8, killsLocEnd));
-		double deaths = Double.parseDouble(stats.substring(deathsLoc + 9, deathsLocEnd));
-		double assists = Double.parseDouble(stats.substring(assistsLoc + 10, assistsLocEnd));
-		double kda = (kills + assists) / deaths;
-
-		return username + "\nELO: " + format.format(elo) + "\nW/L: " + format.format(wl) + "%\nKDA: "
-				+ format.format(kda);
+		return message + "```";
 	}
 
 	public String[] splitCommand(String command) {
@@ -124,6 +145,42 @@ public class BotListener extends ListenerAdapter {
 		String cmd = proc[0];
 		String args = command.replaceFirst(cmd + " ", "");
 		return new String[] { cmd, args };
+	}
+
+	public static Map<String, Object> toMap(JSONObject object) {
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		Iterator<String> keysItr = object.keySet().iterator();
+		while (keysItr.hasNext()) {
+			String key = keysItr.next();
+			Object value = object.get(key);
+
+			if (value instanceof JSONArray) {
+				value = toList((JSONArray) value);
+			}
+
+			else if (value instanceof JSONObject) {
+				value = toMap((JSONObject) value);
+			}
+			map.put(key, value);
+		}
+		return map;
+	}
+
+	public static List<Object> toList(JSONArray array) {
+		List<Object> list = new ArrayList<Object>();
+		for (int i = 0; i < array.length(); i++) {
+			Object value = array.get(i);
+			if (value instanceof JSONArray) {
+				value = toList((JSONArray) value);
+			}
+
+			else if (value instanceof JSONObject) {
+				value = toMap((JSONObject) value);
+			}
+			list.add(value);
+		}
+		return list;
 	}
 
 }
--- src/DiscordBot.java
+++ src/DiscordBot.java
@@ -1,14 +1,15 @@
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Game;
 
-public class DiscordBot { 
+public class DiscordBot {
 
 	public static JDA jda;
 
 	public static final int VERSION = 1;
 	public static final String BOT_TOKEN = "MzEyMzQ4NjE5MjE2OTEyMzg1.C_aGcQ.lWJV2QDFO9ZxjuAdQKtmzDFIHbY";
-	public static final String BOT_PREFIX = ">";
+	public static String BOT_PREFIX = ">";
 
 	public static void main(String[] args) {
 
@@ -18,6 +19,11 @@ public class DiscordBot {
 		try {
 			jda = new JDABuilder(AccountType.BOT).addEventListener(new BotListener()).setToken(BOT_TOKEN)
 					.buildBlocking();
+			while (true) {
+				jda.getPresence().setGame(Game.of("Paragon | " + BOT_PREFIX + "help"));
+				Thread.sleep(61000);
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
